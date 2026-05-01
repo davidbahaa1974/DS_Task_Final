69f85bcd2f3267fb471d854b6c2994355c033e2d
david bahaa khalifa <david.bahaa1974@gmail.com>
2026-05-01 23:51:00 +0300

    DS

--- Stack.cpp
+++ Stack.cpp
@@ -45,7 +45,7 @@ else {
     top--;
 }}
 
-void stackArr::pop(int&val)                         //to return the value of the deleted element
+void stackArr::pop(int&val)                         //to return the value of the deleted element by reference
 {
     if (isempty()){
     cout<<"stack is empty";
@@ -66,7 +66,7 @@ void stackArr::peek(){                            //to print the value of the to
     } 
 }
 
-void stackArr::peek(int&val){                      //to return the value of the top
+void stackArr::peek(int&val){                      //to return the value of the top element by reference
     if (isempty()){
         cout<<"stack is empty";
     }
@@ -76,6 +76,10 @@ void stackArr::peek(int&val){                      //to return the value of the 
 }
 
 void stackArr::print(){                          //print all the element to check the implemntation is right
+    if (isempty()){
+        cout<<"stack is empty";
+    }
+     else
     for (int i = top; i >= 0; i--){
         cout<<array[i]<<" ";
     }
@@ -100,45 +104,45 @@ StackLL::~StackLL(){                        //Destructor to free the memory used
 
 //this retuen true if the stack is empty and false if it is not empty
 bool StackLL::isEmpty(){
-return !length;
+    return !length;
 }
 
 
 //to apply LIFO principle i will make it with prepend and removefirst to make O(1)
 
 void StackLL::push(int value ){                  //to add an item to the stack from first
-Node* newnode= new Node;                            
-newnode->next = top ;                            
-newnode->data = value;
-top= newnode;
-length++;
+    Node* newnode= new Node;                            
+    newnode->next = top ;                            
+    newnode->data = value;
+    top= newnode;
+    length++;
 }
 
 
 int StackLL::pop(){                           //to remove an item from the stack and return its value
-if(isEmpty()){                              
-cout<<"Error :the stack is Empty "<<endl;
-return -1;
+    if(isEmpty()){                              
+        cout<<"Error :the stack is Empty "<<endl;
+        return -1;
 }
 
-else {   
-Node* temp= top;
-int val = temp->data;
-top= top->next;
-delete temp;
-length--;
-return val;
+    else {   
+        Node* temp= top;
+        int val = temp->data;
+        top= top->next;
+        delete temp;
+        length--;
+        return val;
 }
 }
 
 
 int StackLL::peek(){                         //to return the value of the top item without removing it
-if(isEmpty()){
-cout<<"Error :the stack is Empty "<<endl;
-return -1;
+    if(isEmpty()){
+        cout<<"Error :the stack is Empty "<<endl;
+        return -1;
 }
-else
-    return top->data;
+    else
+        return top->data;
 
 }
 
